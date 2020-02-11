08de9bd75c63a33468a50b997a14a5c8fe0e0f56
Ben Chavez <ben.m.chavez@gmail.com>
2020-02-11 19:55:20 +0100

    removed login.rb

--- login_info.rb
+++
@@ -1,8 +0,0 @@
-
-# Login information
-
-username = "rosadj.music@gmail.com"
-password = "seasonalfrequencies2019"
-
-# mdma_username = "rosadj.music@gmail.com"
-# mdma_password = "seasonalfrequencies2019"
