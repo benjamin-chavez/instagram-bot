79b245e2ea7e47357d89d3c6dcd3648450031c60
Ben Chavez <ben.m.chavez@gmail.com>
2020-02-11 19:51:41 +0100

    updating folder contents

--- mass_follow_campaign.rb
+++ mass_follow_campaign.rb
@@ -1,10 +1,10 @@
 # INSTAGRAM BOT
 require 'selenium-webdriver'
 require 'open-uri'
-#require_relative 'app/login_info'
+require_relative 'app/login_info'
 
-username = 'mydancemusicapparel'
-password = 'seasonalfrequencies2019'
+username = private_username
+password = private_password
 
 # def login(username, password)
 # create a driver object and navigate to Instagram
@@ -89,7 +89,7 @@ followers_button.click
 sleep(2)
 
 scr1 = driver.find_element(:xpath, '/html/body/div[4]/div/div[2]')
-my_range = (1..5)
+my_range = (1..100)
 my_range.each do |paramater|
   driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", scr1)
   sleep(2)
@@ -209,39 +209,74 @@ p final_array2.size
 # # 3. Go to webpage of a specific user
 # driver.navigate.to "https://www.instagram.com/#{}/"
 # # 4. click th unfollow button
-
+users = 0
 final_array2.each do |account|
   user_account = account[0].to_s
-    unless user_account == "PEOPLE" || user_account == "HASHTAGS"
+    unless user_account == 'PEOPLE' || user_account == 'HASHTAGS'
       driver.navigate.to "https://www.instagram.com/#{user_account}/"
-      sleep (10)
-      begin
-      follow_button = driver.find_element(:xpath, '//*[@id="react-root"]/section/main/div/header/section/div[1]/div[1]/span/span[1]/button')
-      follow_button.click
-      # something which might raise an exception
-      first_picture = driver.find_element(:xpath, '//*[@id="react-root"]/section/main/div/div[4]/article/div[1]/div/div[1]/div[1]/a/div')
       sleep(4)
+      puts ""
+      puts user_account
+      begin
+      # something which might raise an exception
+      # driver.navigate().refresh()
+      first_picture = driver.find_element(:xpath, '//*[@id="react-root"]/section/main/div/div[2]/article/div/div/div[1]/div[1]/a/div')
+      first_picture.click
+      sleep(4)
+      puts 'User has pictures'
+      driver.navigate().refresh()
+      like_pic_button = driver.find_element(:xpath, '//*[@id="react-root"]/section/main/div/div/article/div[2]/section[1]/span[1]/button')
+      like_pic_button.click
+      sleep(2)
       rescue Exception => e
-        puts"user does not have any pictures"
-        # code that deals with some exception
-      else
-        # code that runs only if *no* exception was raised
-        puts 'User has pictures'
+        begin
+        first_picture = driver.find_element(:xpath, '//*[@id="react-root"]/section/main/div/div[3]/article/div[1]/div/div[1]/div[1]/a/div/div[2]')
         first_picture.click
-        driver.navigate().refresh()
         sleep(4)
+        puts 'User has pictures'
+        driver.navigate().refresh()
         like_pic_button = driver.find_element(:xpath, '//*[@id="react-root"]/section/main/div/div/article/div[2]/section[1]/span[1]/button')
         like_pic_button.click
-        sleep(2)
+        rescue Exception => e
+          puts "user does not have any pictures"
+        else
+          puts "user picture was liked2"
+        end
+        # code that deals with some exception
+        # driver.navigate.to "https://www.instagram.com/#{user_account}/"
+        # follow_button = driver.find_element(:xpath, '//*[@id="react-root"]/section/main/div/header/section/div[1]/div[1]/span/span[1]/button')
+        # follow_button.click
+        # sleep(3)
+      # rescue Exception => e
+      #   puts "User does not have follow button"
+      else
+        # code that runs only if *no* exception was raised
+        puts "user picture was liked1"
       ensure
         # ensure that this code always runs, no matter what
         # does not change the final value of the block
-        puts 'test complete 1'
-    end
+      end
+      begin
+      driver.navigate.to "https://www.instagram.com/#{user_account}/"
+      sleep(4)
+      follow_button = driver.find_element(:xpath, '//*[@id="react-root"]/section/main/div/header/section/div[1]/div[1]/span/span[1]/button')
+      follow_button.click
+      sleep(3)
+      rescue Exception => e
+        puts "follow button not found"
+      else
+        puts "account followed"
+      ensure
+        puts "followed block over"
+        users += 1
+        puts users
+      end
       sleep(rand(60))
     end
 end
 
+# //*[@id="react-root"]/section/main/div/header/section/div[2]/button
+# //*[@id="react-root"]/section/main/div/header/section/div[2]/button
 ##############################################################################################################################################################
 # PERFORM ACTIONS ON USER PAGE (FOLLOW AND LIKE PICTURE)
 
